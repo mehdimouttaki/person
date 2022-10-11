a228f39fe247721b33ab8def32d1c20b8859ee06
elmehdi <m.mouttaki@binarios-it.com>
2022-10-11 08:44:38 +0100

    delete annotation ComponentScan for mapper

--- src/main/java/com/elmouttaki/elmehdi/person/ParentsApplication.java
+++ src/main/java/com/elmouttaki/elmehdi/person/ParentsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.elmouttaki.elmehdi.person.Mapper")
 
 public class ParentsApplication {
 
